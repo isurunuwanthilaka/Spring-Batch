7c9ccefb7c02d3e17036d2d215bbd1c1b71efb9a
IsuruNuwanthilaka <isurunuwanthilaka@gmail.com>
2018-11-06 10:19:52 +0530

    Init Project

---
+++ src/main/java/com/millenniumit/cdraggregation/CdrAggregationApplication.java
@@ -0,0 +1,12 @@
+package com.millenniumit.cdraggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdrAggregationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdrAggregationApplication.class, args);
+    }
+}
